e4d74aab2da7aba9f6c987ad9aa978fb18f69bb8
Mattéo ROBER <m.rober@student.helmo.be>
2024-10-04 14:48:11 +0200

    initial commit

---
+++ back-end/src/main/java/com/helmo/greenThumb/GreenThumbApplication.java
@@ -0,0 +1,13 @@
+package com.helmo.greenThumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenThumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenThumbApplication.class, args);
+	}
+
+}
