28aaf7d908400e8a1f3ea4b64adddf774b281802
Mattéo ROBER <m.rober@student.helmo.be>
2024-10-15 17:09:21 +0200

    /api add

--- back-end/src/main/java/com/helmo/greenThumb/controller/PlantController.java
+++ back-end/src/main/java/com/helmo/greenThumb/controller/PlantController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/plants")
+@RequestMapping("/api/plants")
 @CrossOrigin(origins = "http://localhost:5173")
 public class PlantController {
 
