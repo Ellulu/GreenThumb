4effa69416bcdb9e855a057aeb01c9cc5f0a4821
Vullo Enzo <e.vullo@student.helmo.be>
2024-11-22 10:54:42 +0100

    Adding .env files

--- front-end/src/assets/js/firebase.js
+++ front-end/src/assets/js/firebase.js
@@ -7,13 +7,13 @@ import {
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqL9gREXGzgjB-1k9VuDWNHHYeRWOaSaY",
-  authDomain: "greenthumb-54c99.firebaseapp.com",
-  projectId: "greenthumb-54c99",
-  storageBucket: "greenthumb-54c99.firebasestorage.app",
-  messagingSenderId: "977712179231",
-  appId: "1:977712179231:web:4defe8eca3184d3d410d2a",
-  measurementId: "G-Y7FTQ71DCB",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
