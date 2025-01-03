6a62618682d679c9655bf55cb13e7c8a26859c76
Mattéo ROBER <m.rober@student.helmo.be>
2025-01-03 17:59:26 +0100

    remove profile check

--- front-end/src/views/ProfileView.vue
+++ front-end/src/views/ProfileView.vue
@@ -13,19 +13,5 @@ import ProfileDetails from "../components/ProfileDetails.vue";
       class="flex flex-col md:flex-row gap-4 w-[85%] justify-center items-center md:w-full mx-auto md:mx-0 mt-8"
     >
     </div>
-
-    <div v-else class="flex flex-col items-center gap-4 mt-8">
-      <button
-        @click="toggleSubscription"
-        :class="{
-          'bg-green-600 hover:bg-green-700': !isSubscribed,
-          'bg-red-600 hover:bg-red-700': isSubscribed
-        }"
-        class="text-white rounded-md drop-shadow-md p-3 transition-all ease-in-out duration-200"
-      >
-        <component :is="isSubscribed ? UserCheck : UserPlus" class="h-5 w-5 inline-block mr-2" />
-        {{ isSubscribed ? "Se désabonner" : "S'abonner" }}
-      </button>
-    </div>
   </div>
 </template>
